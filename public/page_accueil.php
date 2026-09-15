d231be711a9483491dd454e7ba444b947bbeaca1
anais <anaiskg@outlook.fr>
2026-09-15 12:23:03 +0200

    refactor(): debut de création de la page d'accueil

---
+++ public/page_accueil.php
@@ -0,0 +1,39 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Site école</title>
+    <link href="accueil.css" rel="stylesheet">
+</head>
+<body>
+<input type="button" id="menu-toggle">
+
+<!-- Bouton -->
+<label for="menu-toggle" class="toggle-btn">☰ Menu</label>
+
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg py-3" id="navbar">
+    <div class="container-fluid">
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link" href="#home">Accueil</a></li>
+            <li class="nav-item"><a class="nav-link" href="#about">À Propos</a></li>
+            <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
+            <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+        </ul>
+    </div>
+</nav>
+
+<!-- ================= FOOTER ================= -->
+<footer class="text-white py-4 mt-5">
+    <div class="container text-center">
+        <p class="mb-2 fs-5">
+            © 2025 – Tous droits réservés Anaïs Kriegel--Grapain - Malik Ben Mechichi -
+            Ahmed Ben Mechichi - Tom Treillon
+        </p>
+    </div>
+</footer>
+</body>
+</html>
