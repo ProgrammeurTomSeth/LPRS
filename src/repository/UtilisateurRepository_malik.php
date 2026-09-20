38068e18b709248ea14613c5831d257b0f7da09a
malikbm2075-code <malikbm2075@gmail.com>
2026-09-20 13:53:25 +0200

    ajout du get utilisateur par id dans utilisteur repository et du new utilisateur si l id n estpas trouvé

--- src/repository/UtilisateurRepository_malik.php
+++ src/repository/UtilisateurRepository_malik.php
@@ -7,4 +7,40 @@ class UtilisateurRepository_malik
     public function __construct(){
         $this->connexionbdd = (new Bdd())->getConnexionbdd();
     }
+
+    public function getUtilisateur($id_Utilisateur)
+    {
+        $sql = "SELECT * FROM utilisateur WHERE id_utilisateur = :id_Utilisateur";
+        $req = $this->connexionBdd->prepare($sql);
+        $req->bindValue(':id_Utilisateur', $id_Utilisateur);
+        $req->execute();
+        $result = $req->fetch();
+
+        if (!$result) {
+            return null;
+        }
+
+        $utilisateur = new Utilisateur(
+            $result['id_utilisateur'],
+            $result['nom_Utilisateur'],
+            $result['prenom_Utilisateur'],
+            $result['mail'],
+            $result['mdp'],
+            $result['telephone'],
+            $result['date_naissance'],
+            $result['role'],
+            $result['statut_validation'],
+            $result['cv'],
+            $result['annee_promo'],
+            $result['id_formation'],
+            $result['specialite'],
+            $result['motif_inscription'],
+            $result['id_entreprise'],
+            $result['poste'],
+            $result['id_gestionnaire_createur'],
+            $result['date_inscription'],
+
+        );
+        return $utilisateur;
+    }
 }
