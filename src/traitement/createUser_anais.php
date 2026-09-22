4794ca364dd0f1f85f99a981585efd0ec1fa7b7e
anais <anaiskg@outlook.fr>
2026-09-22 10:04:32 +0200

    refactor(): modification de la connexion à la base de données

--- src/traitement/createUser_anais.php
+++ src/traitement/createUser_anais.php
@@ -1,12 +1,12 @@
 <?php
 class createUserAnais {
-    private $pdo;
+    private PDO $bdd;
 
     public function __construct() {
-        $this->pdo = Bdd::getConnection();
+        $this->bdd= (new bdd)->getConnexionBdd();
     }
     public function add(Utilisateur_Malik $u) {
-        $stmt = $this->pdo->prepare("INSERT INTO utilisateur 
+        $stmt = $this->bdd->prepare("INSERT INTO utilisateur 
             (id_utilisateur,nom, prenom, email, mdp, telephone, date_naissance, role, statut_validation, cv, annee_promo, id_formation, specilaite, motif_inscription, id_entreprise, poste, id_gestionnaire_utilisateur, date_inscription )
             VALUES (?, ?, ?, ?, ?, ?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
         $stmt->execute([
@@ -29,7 +29,7 @@ class createUserAnais {
             $u->getDateInscription()
 
         ]);
-        $u->setIdUtilisateur($this->pdo->lastInsertId());
+        $u->setIdUtilisateur($this->bdd->lastInsertId());
         return $u->getIdUtilisateur();
     }
 }
