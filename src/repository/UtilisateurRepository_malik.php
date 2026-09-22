11f2f31d1c373c595b0aac3597699b49e8163840
Malik <malikbm2075@gmail.com>
2026-09-22 09:55:17 +0200

    changement du repository de get all utilisateur et debut duajout utilisateur

--- src/repository/UtilisateurRepository_malik.php
+++ src/repository/UtilisateurRepository_malik.php
@@ -81,34 +81,31 @@ class UtilisateurRepository_malik
 
     public function getAllUtilisateurs(){
         $sql = "SELECT * FROM utilisateur";
-        $req = $this->connexionbdd->prepare($sql);
+        $req = $this->connexionBdd->prepare($sql);
         $req->execute();
         $results = $req->fetchAll();
         $tabUtilisateurs = array();
         foreach ($results as $result) {
-            $utilisateur = new Utilisateur(
-                $result['id_utilisateur'],
-                $result['nom_Utilisateur'],
-                $result['prenom_Utilisateur'],
-                $result['mail'],
-                $result['mdp'],
-                $result['telephone'],
-                $result['date_naissance'],
-                $result['role'],
-                $result['statut_validation'],
-                $result['cv'],
-                $result['annee_promo'],
-                $result['id_formation'],
-                $result['specialite'],
-                $result['motif_inscription'],
-                $result['id_entreprise'],
-                $result['poste'],
-                $result['id_gestionnaire_createur'],
-                $result['date_inscription'],
-
-            );
+            $utilisateur = new Utilisateur($result['id_utilisateur'], $result['nom_utilisateur'], $result['prenom_utilisateur'], $result['mail'], $result['mdp'], $result['telephone'], $result['date_naissance'], $result['role'], $result['statut_validation'], $result['cv'], $result['annee_promo'], $result['id_formation'], $result['specialite'], $result['motif_inscription'], $result['id_entreprise'], $result['poste'], $result['id_gestionnaire_createur'], $result['date_inscription']);
             $tabUtilisateurs[] = $utilisateur;
         }
         return $tabUtilisateurs;
+    }
+
+    public function ajouterUtilisateur(Utilisateur $utilisateur){
+        $sql = "INSERT INTO utilisateur (id_utilisateur,nom_utilisateur, prenom_utilisateur, mail, mdp, telephone, date_naissance, role, etatut_validation, cv,annee_promo,)
+                VALUES (:nom, :prenom, :email, :mdp, :telephone, :adresse, :dateNaissance, :role, :etatCompte, :dateCreation)";
+        $req = $this->connexionBdd->prepare($sql);
+        $req->bindValue(':nom',           $utilisateur->getNom());
+        $req->bindValue(':prenom',        $utilisateur->getPrenom());
+        $req->bindValue(':email',         $utilisateur->getEmail());
+        $req->bindValue(':mdp',           $utilisateur->getMdp());
+        $req->bindValue(':telephone',     $utilisateur->getTelephone());
+        $req->bindValue(':adresse',       $utilisateur->getAdresse());
+        $req->bindValue(':dateNaissance', $utilisateur->getDateNaissance());
+        $req->bindValue(':role',          $utilisateur->getRole());
+        $req->bindValue(':etatCompte',    $utilisateur->getEtatCompte());
+        $req->bindValue(':dateCreation',  $utilisateur->getDateCreation());
+        $req->execute();
     }
 }
