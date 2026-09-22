32b8a9332518e425c3b6c6999f78b5e00a17fcff
Malik <malikbm2075@gmail.com>
2026-09-22 11:01:23 +0200

    ajout du modifier utillisateur dans le repository utilisatuer

--- src/repository/UtilisateurRepository_malik.php
+++ src/repository/UtilisateurRepository_malik.php
@@ -93,8 +93,8 @@ class UtilisateurRepository_malik
     }
 
     public function ajouterUtilisateur(Utilisateur $utilisateur){
-        $sql = "INSERT INTO utilisateur (id_utilisateur,nom_utilisateur, prenom_utilisateur, mail, mdp, telephone, date_naissance, role, etatut_validation, cv,annee_promo,id_formation,specialite,motif,inscription,poste,id_gestionnaire_createur,date_inscription)
-                VALUES (:id_utilisateur,:nom_utilisateur, :prenom_utilisateur, :mail, :mdp, :telephone, :date_naissance, :role, :etatut_validation, :cv,:annee_promo,:id_formation,:specialite,:motif,:inscription,:poste,:id_gestionnaire_createur,:date_inscription)";
+        $sql = "INSERT INTO utilisateur (id_utilisateur,nom_utilisateur, prenom_utilisateur, mail, mdp, telephone, date_naissance, role, statut_validation, cv,annee_promo,id_formation,specialite,motif,inscription,poste,id_gestionnaire_createur,date_inscription)
+                VALUES (:id_utilisateur,:nom_utilisateur, :prenom_utilisateur, :mail, :mdp, :telephone, :date_naissance, :role, :statut_validation, :cv,:annee_promo,:id_formation,:specialite,:motif,:inscription,:poste,:id_gestionnaire_createur,:date_inscription)";
         $req = $this->connexionBdd->prepare($sql);
         $req->bindValue(':id_utilisateur',           $utilisateur->getid_utilisateur());
         $req->bindValue(':nom_utilisateur',           $utilisateur->getnom_utilisateur());
@@ -104,7 +104,30 @@ class UtilisateurRepository_malik
         $req->bindValue(':telephone',     $utilisateur->gettelephone());
         $req->bindValue(':date_naissance', $utilisateur->getDate_naissance());
         $req->bindValue(':role',          $utilisateur->getrole());
-        $req->bindValue(':etatut_validation', $utilisateur->getetatut_validation());
+        $req->bindValue(':statut_validation', $utilisateur->getstatut_validation());
+        $req->bindValue(':cv',         $utilisateur->getcv());
+        $req->bindValue(':annee_promo',   $utilisateur->getannee_promo());
+        $req->bindValue(':id_formation', $utilisateur->getid_formation());
+        $req->bindValue(':specialite', $utilisateur->getspecialite());
+        $req->bindValue(':motif_inscription', $utilisateur->getmotif_inscription());
+        $req->bindValue(':id_entreprise', $utilisateur->getid_entreprise());
+        $req->bindValue(':poste', $utilisateur->getposte());
+        $req->bindValue(':id_gestionnaire', $utilisateur->getid_gestionnaire());
+        $req->bindValue(':date_inscription', $utilisateur->getdate_inscription());
+        $req->execute();
+    }
+    public function modifierUtilisateur(Utilisateur $utilisateur){
+        $sql = "UPDATE utilisateur SET id_utilisateur = :id_utilisatuer, nom_utilisateur = :nom_utilisateur, prenom_utilisateur = :prenom_utilisateur, mail = :mail, mdp = :mdp, telephone = :telephone, date_naissance = :date_naissance, role = :role, statut_validation = :statut_validation,cv = :cv,annee_promo = :annee_promo,id_formation = :id_formation,specialite = :specialite,motif = :motif,inscription = :inscription,poste = :poste,id_gestionnaire_createur = :id_gestionnaire_createur,date_inscription = :date_inscription  WHERE id_utilisateur = :idUtilisateur";
+        $req = $this->connexionBdd->prepare($sql);
+        $req->bindValue(':id_utilisateur',           $utilisateur->getid_utilisateur());
+        $req->bindValue(':nom_utilisateur',           $utilisateur->getnom_utilisateur());
+        $req->bindValue(':prenom_utilisateur',        $utilisateur->getprenom_utilisateur());
+        $req->bindValue(':mail',         $utilisateur->getmail());
+        $req->bindValue(':mdp',           $utilisateur->getmdp());
+        $req->bindValue(':telephone',     $utilisateur->gettelephone());
+        $req->bindValue(':date_naissance', $utilisateur->getDate_naissance());
+        $req->bindValue(':role',          $utilisateur->getrole());
+        $req->bindValue(':statut_validation', $utilisateur->getstatut_validation());
         $req->bindValue(':cv',         $utilisateur->getcv());
         $req->bindValue(':annee_promo',   $utilisateur->getannee_promo());
         $req->bindValue(':id_formation', $utilisateur->getid_formation());
