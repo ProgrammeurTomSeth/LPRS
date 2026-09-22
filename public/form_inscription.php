8fdc6e491f5bd67e52cd1f64b9c18f4fc9c4616c
anais <anaiskg@outlook.fr>
2026-09-22 11:37:08 +0200

    feat(): création du formulaire d'inscription.Le css est à modifier.

---
+++ public/form_inscription.php
@@ -0,0 +1,33 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulaire d'inscritpion</title>
+    <link href="form_inscription.css" rel="stylesheet">
+</head>
+<body>
+<form action="traitement_inscription.php" method="POST" enctype="multipart/form-data">
+
+    <label>Nom :</label>
+    <input type="text" name="nom" required>
+
+    <label>Prénom :</label>
+    <input type="text" name="prenom" required>
+
+    <label>Email :</label>
+    <input type="email" name="email" required>
+
+    <label>Mot de passe :</label>
+    <input type="password" name="mdp" required>
+
+    <label>Téléphone :</label>
+    <input type="text" name="telephone">
+
+
+    <button type="submit">Créer le compte</button>
+</form>
+
+</body>
+</html>
